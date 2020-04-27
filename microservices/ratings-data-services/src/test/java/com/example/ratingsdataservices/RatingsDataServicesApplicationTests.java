4aced1cdf340a48e3c297b2675aa6fde36240818
Jayant Rai <jayant.chamling@outlook.com>
2020-04-27 17:21:26 -0500

    initialized springboot microservices

---
+++ microservices/ratings-data-services/src/test/java/com/example/ratingsdataservices/RatingsDataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratingsdataservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
